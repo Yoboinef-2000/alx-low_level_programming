386ae2d5031e02cbed54feed707e9275c987fa3c
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-05 10:34:48 -0700

    Let's see if this works

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Error)
  */
-
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59); 
-
-    return 1; 
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
-
