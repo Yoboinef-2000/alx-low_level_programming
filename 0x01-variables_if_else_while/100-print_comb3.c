e18a2624a9f6d47080f43bc20d29bd3d37b9a2d8
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-06 16:22:56 -0700

    More exercisessss

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+	int j;
+
+        for (i = 0; i <= 9; i++)
+        {
+		for (j = 1; j <= 9; j++)
+		{
+			
+			if (i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '1');
+			}
+		       
+			if (i != 9 && j != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+        }
+
+        putchar('\n');
+        return (0);
+}
