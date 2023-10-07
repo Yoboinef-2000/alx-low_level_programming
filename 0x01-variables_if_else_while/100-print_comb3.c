31b5540f5b3e7aa5b0f5ff63e34c91f0f98d7d2b
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-06 17:31:13 -0700

    More exerciSeS

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,23 @@
  */
 int main(void)
 {
-        int i;
+	int i;
 	int j;
 
-        for (i = 0; i <= 9; i++)
-        {
-		for (j = 1; j <= 9; j++)
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
 		{
+			putchar(i + '0');
+			putchar(j + '0');
 			
-			if (i < j)
-			{
-				putchar(i + '0');
-				putchar(j + '1');
-			}
-		       
-			if (i != 9 && j != 8)
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
         }
-
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
