6e722e81897e262d58a6cd75ff4b8585e89b189d
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-10 02:52:16 -0700

    Simple exercises

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* This is just a replica of the putchar function */
+int _putchar(char c)
+{
+	putchar(c);
+	return (c);
+}
+
+/* This will list all the alphabets in order */
+void print_alphabet(void)
+{
+	char alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		alph++;
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains more C exercises. It will mostly be about functions and nested loops
