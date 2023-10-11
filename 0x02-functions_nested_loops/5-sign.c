2f4efdb80a57e9e98ba5f3d082595542b09c4698
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-11 04:40:48 -0700

    Let's see if this works

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,18 +13,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
-		return 1;
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
-		return -1;
+		_putchar('-');
+		return (-1);
 	}
 }
 
