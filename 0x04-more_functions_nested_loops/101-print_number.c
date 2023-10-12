a9c1ffdbb01c9961ff75c6192d3855f1f53a2f27
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-12 14:37:03 -0700

    Moww

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include"main.h"
+#include<stdio.h>
 
 /**
  * print_number - Prints an integer using _putchar.
  * @n: The integer to print.
- *
- * Description: Print dat thang
  */
+
 void print_number(int n)
 {
 	if (n < 0)
@@ -13,26 +13,10 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n == 0)
+	if (n / 10 != 0)
 	{
-		_putchar('0');
+		print_number(n / 10);
 	}
-	else
-	{
-		int rev = 0;
-		int digit;
-
-		while (n > 0)
-		{
-			digit = n % 10;
-			rev = rev * 10 + digit;
-			n = n / 10;
-		}
-		while (rev > 0)
-		{
-			digit = rev % 10;
-			_putchar('0' + digit);
-			rev = rev / 10;
-		}
-	}
+	_putchar('0' + n % 10);
 }
+
