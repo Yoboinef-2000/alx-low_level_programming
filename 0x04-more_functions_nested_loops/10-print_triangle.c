83f855e240bac7c66e667ad00e2495bb642af2aa
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-12 13:52:21 -0700

    Hola Comoestas?

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * print_triangle - Prints a triangle followed by a new line.
+ * @size: The size of the triangle.
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= size - i)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
