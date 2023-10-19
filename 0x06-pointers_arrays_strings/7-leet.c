d283cbecee6dadaabe49412b92348f98d6bec6f2
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-19 15:29:40 -0700

    More exercises

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,5 +17,36 @@
  */
 char *leet(char *s)
 {
-return (s);
+	int i;
+	char c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		c = s[i];
+
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		{
+			if (c == 'a' || c == 'A')
+			{
+				s[i] = '4';
+			}
+			else if (c == 'e' || c == 'E')
+			{
+				s[i] = '3';
+			}
+			else if (c == 'o' || c == 'O')
+			{
+				s[i] = '0';
+			}
+			else if (c == 't' || c == 'T')
+			{
+				s[i] = '7';
+			}
+			else if (c == 'l' || c == 'L')
+			{
+				s[i] = '1';
+			}
+		}
+	}
+	return (s);
 }
