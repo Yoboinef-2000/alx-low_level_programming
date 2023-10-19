a0e6d6babaaa3dfd0c6035031ad85d6af9253349
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-19 12:07:31 -0700

    More exercises

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - this function capitalizes every
+ * first word in a given string.
+ * @s: the string
+ *
+ * Description: as mentioned in the first commented
+ * out line, this function capitalizes every first word
+ * in a given string. Word separators include
+ * space, tabulation, new line, ,, ;, ., !, ?, '"', (, ), {, and }
+ *
+ * Return: a string with every word in it capitalized is
+ * returned
+ */
+
+char *cap_string(char *s)
+{
+	int cp;
+
+	cp = 27;
+
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		char c = s[i];
+
+		if (c >= 'a' && c <= 'z' && cp == 27)
+		{
+			s[i] = c + ('A' - 'a');
+			cp = 0;
+		}
+
+		else if (c == ' ' || c == '\t' || c == '\n' || c == ',' ||
+			c == ';' || c == '.' || c == '!' || c == '?'
+			|| c == '\"' || c == '(' || c == ')' || c == '{' ||
+			c == '}')
+		{
+			cp = 27;
+		}
+
+		else
+		{
+		       cp = 0;
+		}
+	}
+	return (s);
+}
