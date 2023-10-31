f70cb708a2baa555da146cc1a4dac0d753e8a4d1
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-10-31 01:18:41 -0700

    More malloc exercises

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,9 +24,13 @@ char *str_concat(char *s1, char *s2)
 
 	conctdstrs = malloc((sizeof(char) * (strlen(s1) + strlen(s2))) + 1);
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	if (conctdstrs == NULL)
 	{
