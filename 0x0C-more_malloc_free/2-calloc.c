5ad114a2e01b11791bae791d195bc7f427d515c2
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-02 11:00:03 -0700

    More malloc exercises - Pretty elementary thus far

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - this is a function that allocates
+ * memory for an array using malloc
+ * @nmemb: This is the number of elments in the array
+ * @size: This is the size in bytes of the elements
+ *
+ * Description: The _calloc function allocates memory for
+ * an array of "nmemb" elements of "size" bytes each
+ *
+ * Return: Returns a pointer to the allocated memory
+ * If nmemb or size is 0, then _calloc returns NULL
+ * If malloc fails, then _calloc returns NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *allctdmem;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	allctdmem = malloc(nmemb * size);
+
+	if (allctdmem == NULL)
+	{
+		return (NULL);
+	}
+	return (allctdmem);
+}
