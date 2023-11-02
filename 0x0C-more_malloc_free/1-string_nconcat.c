fa9f61a335070af70581f4aaa298fe2b9d480960
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-02 09:24:40 -0700

    More malloc exercises

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,6 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+	if (n >= strlen(s2))
+	{
+		n = strlen(s2);
+	}
 	conctdstr = malloc(strlen(s1) + n + 1);
 	if (conctdstr == NULL)
 	{
