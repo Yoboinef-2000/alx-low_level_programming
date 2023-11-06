37b6afdfcc54744ac31f4242507c91e8633c3101
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-06 03:42:03 -0800

    More structure exercises

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,17 +20,24 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		d->name = "NIL";
+		d->name = "(nil)";
 		printf("Name: %s\n", d->name);
 	}
 	else
 	{
 		printf("Name: %s\n", d->name);
 	}
-	printf("Age: %f\n", d->age);
+	if (d->age >= 0)
+	{
+		printf("Age: %f\n", d->age);
+	}
+	else
+	{
+		printf("Age: (nil)\n");
+	}
 	if (d->owner == NULL)
 	{
-		d->owner = "NIL";
+		d->owner = "(nil)";
 		printf("Owner: %s\n", d->owner);
 	}
 	else
