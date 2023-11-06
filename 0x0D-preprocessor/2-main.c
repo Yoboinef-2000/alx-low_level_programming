23060d8f8c1d6277f2cb0a33dca2cee82601904a
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-06 01:52:12 -0800

    I thought I betty checked my code, when I didn't. Had my had spinning for a while

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -6,11 +6,11 @@
  * Description: This is a program that prints the name of the file
  * it was compiled from
  *
- * Return - 0
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __BASE_FILE__);
 	return (0);
 }
