ca6826b532a4119f484da1989b9fe791201e7e53
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-06 05:21:02 -0800

    More exercises

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - this is a function that frees the space
+ * allocated to dog_t by malloc
+ * FREE MY DOGS
+ *
+ * Description: refer to the first commented out line
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
