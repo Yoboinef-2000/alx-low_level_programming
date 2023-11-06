a2ece9b933d0f0d7188744e63bc4670d5442cdef
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-06 03:35:37 -0800

    More exercises

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,6 +6,10 @@
  * @d: pointer to the strucutre dog
  *
  * Description: Refer to the first commented out line
+ *
+ * Return: If an element of d is NULL, print (nil) instead of
+ * that element. (if name is NULL, print Name: (nil))
+ * If d is NULL print nothing.
  */
 
 void print_dog(struct dog *d)
@@ -17,12 +21,20 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 	{
 		d->name = "NIL";
+		printf("Name: %s\n", d->name);
 	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
 		d->owner = "NIL";
+		printf("Name: %s\n", d->owner);
 	}
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
