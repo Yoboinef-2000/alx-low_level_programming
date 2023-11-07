82436d46fd351d01310968f86a850457d3926d75
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-07 15:24:23 -0800

    Second attempt - Back for more points

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -6,6 +6,7 @@
  * free_dog - this is a function that frees the space
  * allocated to dog_t by malloc
  * FREE MY DOGS
+ * @d: the dog to be freed(ie the memory allocated)
  *
  * Description: refer to the first commented out line
  */
