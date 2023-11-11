ffd0be2a0a6efa45e97545e446bb468ab2e02a64
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-10 18:10:15 -0800

    A lil tough

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,11 +2,12 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
-typedef struct
-{
-	char format;
-	void (*print_function)(va_list args);
-} FormatSpecifier;
+/**
+ * print_char - this function prints characters
+ * @args: holds information about the varaible arguments
+ *
+ * Description: char
+ */
 
 void print_char(va_list args)
 {
@@ -16,6 +17,13 @@ void print_char(va_list args)
 	printf("%c", c);
 }
 
+/**
+ * print_int - this function prints integers
+ * @args: holds information about the varaible arguments
+ *
+ * Description: integers
+ */
+
 void print_int(va_list args)
 {
 	int num;
@@ -24,13 +32,27 @@ void print_int(va_list args)
 	printf("%d", num);
 }
 
+/**
+ * print_float - this function prints floats
+ * @args: holds information about the varaible arguments
+ *
+ * Description: float
+ */
+
 void print_float(va_list args)
 {
-	double f; 
+	double f;
 
 	f = va_arg(args, double);
 	printf("%f", f);
 }
+
+/**
+ * print_string - this function prints strings
+ * @args: holds information about the varaible arguments
+ *
+ * Description: strings
+ */
 
 void print_string(va_list args)
 {
@@ -40,20 +62,32 @@ void print_string(va_list args)
 	printf("%s", (str != NULL) ? str : "(nil)");
 }
 
+/**
+ * print_all - this function prints all
+ * (characters, integers, floats, strings)
+ * @format: this is a list of types of arguments
+ * passed to the function
+ * "c": char
+ * "i": integer
+ * "f": float
+ * "s": char * (if the string is NULL, print (nil) instead
+ *
+ * Description: This function prints all
+ */
 
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i, j;
-	int is_first; 
-	
+	int is_first;
+
 	FormatSpecifier formats[] = {
-                {'c', print_char},
-                {'i', print_int},
-                {'f', print_float},
-                {'s', print_string},
-                {0, NULL}
-        };
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{0, NULL}
+	};
 	is_first = 1;
 	i = 0;
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,12 @@
 
 #include <stdio.h>
 
+typedef struct
+{
+	char format;
+	void (*print_function)(va_list args);
+} FormatSpecifier;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
