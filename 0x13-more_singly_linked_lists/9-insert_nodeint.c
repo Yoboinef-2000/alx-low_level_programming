1a39a7da54f8e011800854e87802a196eef54da5
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-21 12:22:44 -0800

    Vele vele right down - Or atleast that is what I think he is saying

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *current, *newnode;
 
-	if (*head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
