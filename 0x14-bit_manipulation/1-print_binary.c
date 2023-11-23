456f6c52b8302571cab2fb0793dd369d916e08e2
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2023-11-23 11:59:46 -0800

    We the big three like we started the league

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * print_binary_recursive - this fucntion prints the binary representation
+ * of a number
+ *
+ * @n: This is the number that is going to be converted
+ *
+ * Description: refer to the first commeted out line
+ */
+
+void print_binary_recursive(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary_recursive(n >> 1);
+	}
+	_putchar((n & 1) + '0');
+}
 
 /**
  * print_binary - this fucntion prints the binary representation
@@ -9,34 +26,16 @@
  * @n: This is the number that is going to be converted
  *
  * Description: refer to the first commeted out line
- *
- * Return: returns the binary representation of a number
  */
 
 void print_binary(unsigned long int n)
 {
-	int shift;
-	unsigned long int mask;
-
-	mask = 1UL << shift;
-	shift = sizeof(unsigned long int) * 8 - 1;
-
-	while (shift >= 0)
+	if (n == 0)
 	{
-		if ((n & mask) == 0)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-		}
-		shift--;
-		if (shift % 4 == 0)
-		{
-			_putchar(' ');
-		}
-		mask >>= 1;
+		_putchar('0');
 	}
-	_putchar('\n');
+	else
+	{
+		print_binary_recursive(n);
+	}
 }
