121dbc66ad3add7f181584d060840822213ee16e
Neftalem Mussie Gebremicael <nhagos2000@gmail.com>
2024-02-02 07:16:03 -0800

    AniTTA MAX WYnn

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create- this function creates a hash table.
+ *
+ * @size: is the size of the array.
+ *
+ * Description: refer to the first commented out line.
+ *
+ * Return: returns a pointer to the newly created hash table.
+ * (or)
+ * If something went wrong, your function should return NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashhTable;
+
+	hashhTable = NULL;
+
+	if (size < 1)
+	{
+		return (NULL);
+	}
+	hashhTable = malloc(sizeof(hash_table_t));
+
+	if (hashhTable == NULL)
+	{
+		return (NULL);
+	}
+	hashhTable->array = malloc(sizeof(hash_node_t) * size);
+	if (hashhTable->array == NULL)
+	{
+		free(hashhTable);
+		return (NULL);
+	}
+
+	hashhTable->size = size;
+	return (hashhTable);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This repo will hold introductory exercises on hash tables.
