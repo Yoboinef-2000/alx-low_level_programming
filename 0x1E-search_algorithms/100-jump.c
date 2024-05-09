ab5b9bdb6b6e4e97be4ee55d6b99d6994170ab2f
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-05-09 04:35:25 +0300

    El Gran Varon.

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -43,7 +43,8 @@ int jump_search(int *array, size_t size, int value)
 		}
 		else
 		{
-			printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+            if (low == 0)
+			    printf("Value checked array[%lu] = [%d]\n", low, array[low]);
 			printf("Value checked array[%lu] = [%d]\n", high, array[high]);
 		}
 		if ((high + jumpstep) > size)
